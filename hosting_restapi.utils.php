9c9d115e7015d281926e5f7cba09f3ec9feed8ea
Mathieu Lutfy <mathieu@civicrm.org>
2022-09-09 16:43:34 +0000

    Add site/disable method and some cleanup

--- hosting_restapi.utils.php
+++ hosting_restapi.utils.php
@@ -6,6 +6,34 @@
  */
 
 /**
+ * Validate if the REST-ish function exists
+ */
+function hosting_restapi_invoke_function($func, $method) {
+  $result = [];
+  $f = 'hosting_restapi_' . $func . '_' . $method;
+
+  try {
+    if (function_exists($f)) {
+      $result = $f();
+    }
+    else {
+      $result = [
+        'status' => 'error',
+        'message' => "Unknown method for $func: $method",
+      ];
+    }
+  }
+  catch (Exception $e) {
+    $result = [
+      'status' => 'error',
+      'message' => $e->getMessage(),
+    ];
+  }
+
+  return $result;
+}
+
+/**
  * Checks if authentication token is valid for a given site.
  *
  * @param String $site_name
@@ -30,11 +58,41 @@ function hosting_restapi_is_valid_auth_token($url, $token) {
  * @returns Boolean
  */
 function hosting_restapi_get_invoice_id_from_token($url, $token) {
-  if (! $url) {
+  if (!$url) {
     throw new Exception('Missing url argument.');
   }
+  if (!$token) {
+    throw new Exception('Missing token argument.');
+  }
 
-  return db_query('SELECT invoice_id FROM hosting_restapi_order WHERE site = :url AND token = :token', array(':url' => $url, ':token' => $token))->fetchField();
+  $invoice_id = db_query('SELECT invoice_id FROM hosting_restapi_order WHERE site = :url AND token = :token', array(':url' => $url, ':token' => $token))->fetchField();
+
+  if (!$invoice_id) {
+    throw new Exception('url/token authentication failed');
+  }
+
+  return $invoice_id;
+}
+
+/**
+ * Validates the URL/invoice_id combo.
+ * Returns the order_id.
+ */
+function hosting_restapi_validate_invoice_id_for_url($url, $invoice_id) {
+  if (!$url) {
+    throw new Exception('Missing url argument.');
+  }
+  if (!$invoice_id) {
+    throw new Exception('Missing invoice_id argument.');
+  }
+
+  $order_id = db_query('SELECT id FROM hosting_restapi_order WHERE invoice_id = :invoice_id AND site = :url', array(':invoice_id' => $invoice_id, ':url' => $url))->fetchField();
+
+  if (!$order_id) {
+    throw new Exception('url/invoice_id authentication failed');
+  }
+
+  return $order_id;
 }
 
 /**
